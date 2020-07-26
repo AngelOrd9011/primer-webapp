79f42e3f33e3d8bde53ac7cad1c9bdf46b8ef1f8
AngelOrd9011 <m.angelord9011@outlook.com>
2020-07-26 17:53:22 -0500

    quitar imports no usados
    se retiran lineas de codigo de importaciones que no se usaban en productos.component

--- src/app/components/productos.component.ts
+++ src/app/components/productos.component.ts
@@ -1,5 +1,4 @@
 import { Component } from "@angular/core";
-import { Router,ActivatedRoute,Params } from "@angular/router";
 import { ProductoService } from "../services/producto.service";
 import { Producto } from "../models/producto";
 
@@ -12,8 +11,6 @@ export class ProductosListComponent{
     public titulo:string;
     public productos: Producto[];
     constructor(
-        private _route:ActivatedRoute,
-        private _router:Router,
         private _productoService: ProductoService
     ){
         this.titulo='Lista de productos';
